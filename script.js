b58359d091af92f512e4d499fa56eb85c3b31391
Andy Bautista <odfelip.bautista@gmail.com>
2024-08-19 11:51:39 -0500

    Create Gameboard and Player objects

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 // Ready to be scripted!
+// Step 1
+// Store the gameboard as an array inside of a Gameboard object
+
+const Gameboard = () => {
+    const board = [];
+    return {board};
+}
+
+// Regular object way
+// function Player(name) {
+//     this.name = name;
+// }
+
+// const dingus = new Player('Andy');
+
+// Factory function way
+const Player = (name, symbol) => {
+    const saySymbol = () => console.log(`${name}\'s symbol is ${symbol}`);
+    return { name, saySymbol};
+};
+
+const tictactoe = Gameboard();
+const andy = Player('Andy', 'X');
+const brian = Player('Brian', 'O');
+
+console.log(tictactoe);
+console.log(andy.name);
+andy.saySymbol();
+
+console.log(brian.name);
+brian.saySymbol();
