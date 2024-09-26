ea9b5b39f3223b310604715121764a2a36abc293
Andy Bautista <odfelip.bautista@gmail.com>
2024-09-26 12:44:43 -0500

    Create main factory function + tuck in other code

--- script.js
+++ script.js
@@ -2,9 +2,15 @@
 // Step 1
 // Store the gameboard as an array inside of a Gameboard object
 
-const Gameboard = () => {
+function Gameboard() {
     const board = [];
-    return {board};
+
+    function createPlayer (name, symbol) {
+        const saySymbol = () => console.log(`${name}\'s symbol is ${symbol}`);
+        return {name, saySymbol};
+    };
+
+    return {board, createPlayer};
 }
 
 // Regular object way
@@ -15,14 +21,14 @@ const Gameboard = () => {
 // const dingus = new Player('Andy');
 
 // Factory function way
-const Player = (name, symbol) => {
-    const saySymbol = () => console.log(`${name}\'s symbol is ${symbol}`);
-    return { name, saySymbol};
-};
+// function createPlayer (name, symbol) {
+//     const saySymbol = () => console.log(`${name}\'s symbol is ${symbol}`);
+//     return {name, saySymbol};
+// };
 
 const tictactoe = Gameboard();
-const andy = Player('Andy', 'X');
-const brian = Player('Brian', 'O');
+const andy = tictactoe.createPlayer('Andy', 'X');
+const brian = tictactoe.createPlayer('Brian', 'O');
 
 console.log(tictactoe);
 console.log(andy.name);
